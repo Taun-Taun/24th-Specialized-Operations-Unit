b0db44d9d3e16b193c1657efcbc262e750603f7e
Taun-Taun <138324901+Taun-Taun@users.noreply.github.com>
2023-12-07 19:08:49 +0000

    Testing shizzzzzzz

--- Aux24th/Infantry/Config.cpp
+++ Aux24th/Infantry/Config.cpp
@@ -29,7 +29,9 @@ class CfgWeapons
     class V_PlateCarrier1_rgr;
     class lsd_gar_airborne_new;
     class ItemInfo;
-    class JLTS_CloneHelmetARC_illum;
+    class JLTS_Clone_P2_ARC;
+    class JLTS_CloneVestOfficer2;
+    class JLTS_CloneHelmetP2;
     class SWLB_clone_basic_armor: V_PlateCarrier1_rgr
     {
         class ItemInfo;
@@ -703,7 +705,7 @@ class CfgWeapons
 		};
 	};
 
-    class Aux24th_41st_ARCHelmetTest: JLTS_CloneHelmetARC_illum							
+    class Aux24th_41st_ARCHelmetTest: JLTS_Clone_P2_ARC						
 	{
 		author="41stEliteCorps";										
 		scope=2;												
@@ -719,7 +721,98 @@ class CfgWeapons
 			"Aux24th\Textures\41st_ARC_Helmet.paa"
 		};
 	};
+    class W41_CSM_VEST: JLTS_CloneVestOfficer2				
+	{
+		author="41stEliteCorps";										
+		scope=2;
+		scopeCurator = 2;
+		displayName="41st CSM Vest";
+		hiddenSelectionsTextures[]= {"W41_Aux_Armor3\Data\VEST\OFFICER_VEST.paa",};
+		class ItemInfo: ItemInfo {
+          containerClass="Supply200";
+        };
+	};
+    class W41_CT_Helmet: JLTS_CloneHelmetP2						
+	{
+		author="41stEliteCorps";										
+		scope=2;												
+		scopeCurator = 2;
+		displayName="[41st] Rank Helmet (CT)";
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_CT_Helmet.paa" };
+	};
+    class W41_SCT: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (SCT)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_SCT_Helmet.paa" };
+	};
+    class W41_VCT: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (VCT)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_VCT_Helmet.paa" };
+	};
+    class W41_CLC: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (CLC)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_CLC_Helmet.paa" };
+	};
+    class W41_CSP: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (CSP)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_CSP_Helmet.paa" };
+	};
+    class W41_CP: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (CP)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_CP_Helmet.paa" };
+	};
+    class W41_WO1: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (WO1)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_WO1_Helmet.paa" };
+	};
+    class W41_CS: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (CS)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_CS_Helmet.paa" };
+	};
+    class W41_WO2: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (WO2)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_WO2_Helmet.paa" };
+	};
+    class W41_CGS: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (CGS)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_CGS_Helmet.paa" };
+	};
+    class W41_WO3: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (WO3)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_WO3_Helmet.paa" };
+	};
+    class W41_CSM: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (CSM)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_CSM_Helmet.paa" };
+	};
+    class W41_WO4: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (WO4)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_WO4_Helmet.paa" };
+	};
+    class W41_CompSM: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (CompSM)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_CompSM_Helmet.paa" };
+	};
+    class W41_WO5: W41_CT_Helmet							
+	{
+		displayName="[41st] Rank Helmet (WO5)";									
+		hiddenSelectionsTextures[]= { "Aux24th\Textures\41st_WO5_Helmet.paa" };
+	};
+
 };
+
 
 
 class CfgVehicles
@@ -1014,4 +1107,621 @@ class CfgVehicles
             "ls_armor_bluefor\uniform\gar\phase2\data\undersuit_co.paa" // Undersuit / Body Glove
         };
     };
+};
+
+class CfgPatches
+{
+	class LF_Weapon_Unit_WestarM5
+	{
+		author="Last Force Project";
+		units[]={};
+		weapons[]={};
+		requiredAddons[]=
+		{
+			"a3_anims_f",
+			"a3_weapons_f"
+		};
+	};
+};
+class CfgAmmo
+{
+	class Aux24th_Ammo_Base;
+	class Aux24th_ammo_he;
+	class Aux24th_ammo_westar_gr: Aux24th_ammo_he
+	{
+		submunitionammo="ammo_Penetrator_Titan_AT";
+		indirectHit=2;
+		indirectHitRange=3;
+		hit=1000;
+		model="LF_Weapon_Unit\main\laser_red.p3d";
+		typicalSpeed=1550;
+		explosive=1;
+		effectsMissile="missile3";
+		caliber=100;
+		submunitionInitSpeed=1000;
+		submunitionParentSpeedCoef=0;
+		effectfly="Aux24th_plasma_red";
+		muzzleEffect = "BIS_fnc_effectFiredRifle";
+	};
+	class Aux24th_ammo_westar_gl: Aux24th_ammo_he
+	{
+		submunitionammo="ammo_Penetrator_Titan_AT";
+		indirectHit=20;
+		indirectHitRange=5;
+		hit=92;
+		tracerscale=2.5;
+		model="LF_Weapon_Unit\main\laser_red.p3d";
+		typicalSpeed=1550;
+		explosive=1;
+		effectsMissile="missile3";
+		caliber=12;
+		submunitionInitSpeed=1000;
+		submunitionParentSpeedCoef=0;
+		effectfly="Aux24th_plasma_red";
+		muzzleEffect = "BIS_fnc_effectFiredRifle";
+	};
+	class Aux24th_ammo_westar_auto: Aux24th_Ammo_Base
+	{
+		hit=26.45;
+		typicalspeed=450;
+		caliber=1.4;
+		model="LF_Weapon_Unit\main\laser_blue.p3d";
+		dangerRadiusBulletClose=4;
+		dangerRadiusHit=8;
+		suppressionRadiusBulletClose=2;
+		suppressionRadiusHit=4;
+		audibleFire=45;
+		airFriction=0;
+		timetolive=6;
+		effectfly="Aux24th_plasma_blue";
+	};
+	class Aux24th_ammo_westar_sniper_red: Aux24th_Ammo_Base
+	{
+		hit=109.25;
+		model="LF_Weapon_Unit\main\laser_blue.p3d";
+		dangerRadiusBulletClose=8;
+		dangerRadiusHit=12;
+		suppressionRadiusBulletClose=8;
+		dangerRadusHit=12;
+		suppressionRadusBulletClose=6;
+		suppressionRadusHit=8;
+		audiblefire=120;
+		brightness=2;
+		airFriction=0;
+		waterFriction=0;
+		caliber=3.8000001;
+		timetolive=20;
+		effectfly="Aux24th_plasma_blue";
+		typicalspeed=2250;
+	};
+};
+class CfgMagazines
+{
+    class Aux24th_mag_base;
+    class Aux24th_WestarM5_Mag: Aux24th_mag_base
+	{
+		author="Last Force Project";
+		scope=2;
+		tracersEvery=1;
+		type=16;
+		mass=10;
+		initSpeed=310;
+		picture="\LF_Weapon_Unit\westarm5a\ui\westar_magazine.paa";
+		displayName="Westar M5 energy plasma cell (100rnd)";
+		ammo="Aux24th_ammo_westar_auto";
+		count=100;
+		descriptionShort="100rnd energy cell for Westar M5";
+		model="LF_Weapon_Unit\westarm5\westarm5mag.p3d";
+	};
+	class AUX24_WestarM5_Mag_Sniper: Aux24th_mag_base
+	{
+		author="Last Force Project";
+		scope=2;
+		tracersEvery=1;
+		type=16;
+		mass=10;
+		initSpeed=2000;
+		picture="\LF_Weapon_Unit\westarm5a\ui\westar_magazine.paa";
+		displayName="Westar M5 sniper high energy plasma cell (10rnd)";
+		ammo="Aux24th_ammo_westar_sniper_red";
+		count=10;
+		descriptionShort="100rnd sniper high energy plasma cell for Westar M5";
+		model="LF_Weapon_Unit\westarm5\westarm5mag.p3d";
+	};
+	class Aux24th_WestarM5_GR: Aux24th_mag_base
+	{
+		author="Last Force Project";
+		scope=2;
+		tracersEvery=1;
+		type=16;
+		mass=5;
+		initSpeed=710;
+		picture="\LF_Weapon_Unit\westarm5a\ui\westar_magazine.paa";
+		displayName="Westar M5 high explosive plasma (3rnd)";
+		ammo="Aux24th_ammo_westar_gr";
+		count=3;
+		descriptionShort="3rnd high explosive plasma cell for Westar M5";
+		model="LF_Weapon_Unit\westarm5\westarm5mag.p3d";
+	};
+	class Aux24th_WestarM5_Mag_GL: Aux24th_mag_base
+	{
+		author="Last Force Project";
+		scope=2;
+		tracersEvery=1;
+		type=16;
+		mass=20;
+		initSpeed=80;
+		picture="\LF_Weapon_Unit\westar55\ui\w55mag.paa";
+		displayName="Westar M5 high explosive plasma (3rnd)";
+		ammo="Aux24th_ammo_westar_gl";
+		count=3;
+		descriptionShort="3rnd high explosive plasma cell for Westar M5";
+	};
+};
+class WeaponSlotsInfo;
+class Cowsslot;
+class MuzzleSlot;
+class Mode_SemiAuto;
+class Mode_FullAuto;
+class CfgWeapons
+{
+	class Rifle;
+	class Aux24th_rifle_base: Rifle
+	{
+		author="Last Force Project";
+		scope=1;
+		class WeaponSlotsInfo
+		{
+			class SlotInfo;
+		};
+		class GunParticles;
+		class AnimationSources;
+	};
+	class Aux24th_WestarM5: Aux24th_rifle_base
+	{
+		scope=2;
+		model="LF_Weapon_Unit\westarm5\westarm5.p3d";
+		handAnim[]=
+		{
+			"OFP2_ManSkeleton",
+			"LF_Weapon_Unit\westarm5a\anims\westarm5a2.rtm"
+		};
+		reloadAction="ReloadMagazine";
+		picture="LF_Weapon_Unit\westarm5a\ui\westarm5standard.paa";
+		magazines[]=
+		{
+			"Aux24th_WestarM5_Mag"
+		};
+		displayname="Westar M5";
+		descriptionShort="Standard Westar-M5";
+		selectionFireAnim="zasleh";
+		class Library
+		{
+			libTextDesc="";
+		};
+		drySound[]=
+		{
+			"LF_Weapon_Unit\main\sounds\dc15_empty.wss",
+			1,
+			1,
+			50
+		};
+		reloadMagazineSound[]=
+		{
+			"LF_Weapon_Unit\main\sounds\dc15_reload.wss",
+			0.8,
+			1,
+			10
+		};
+		soundBullet[]=
+		{
+		};
+		modes[]=
+		{
+			"Single",
+			"FullAuto",
+			"close",
+			"short",
+			"medium"
+		};
+		recoil="Aux24th_recoil";
+		fireLightDuration=0.050000001;
+		fireLightIntensity=0.40000001;
+		fireLightDiffuse[]={0,0,0.0099999998};
+		fireLightAmbient[]={0,0,0};
+		class Single: Mode_SemiAuto
+		{
+			sounds[]=
+			{
+				"StandardSound"
+			};
+			class BaseSoundModeType
+			{
+				weaponSoundEffect="";
+				closure1[]={};
+				closure2[]={};
+				soundClosure[]={};
+			};
+			class StandardSound: BaseSoundModeType
+			{
+				weaponSoundEffect="";
+				begin1[]=
+				{
+					"LF_Weapon_Unit\westarm5\sounds\westarm5_shot.ogg",
+					1,
+					1,
+					1800
+				};
+				soundBegin[]=
+				{
+					"begin1",
+					0.33000001
+				};
+			};
+			reloadTime=0.3;
+			dispersion=0.0001;
+			minRange=5;
+			minRangeProbab=0.30000001;
+			midRange=25;
+			midRangeProbab=0.60000002;
+			maxRange=50;
+			maxRangeProbab=0.1;
+			aiRateOfFire=2;
+			aiRateOfFireDistance=25;
+		};
+		class FullAuto: Mode_FullAuto
+		{
+			sounds[]=
+			{
+				"StandardSound"
+			};
+			class BaseSoundModeType
+			{
+				weaponSoundEffect="";
+				closure1[]={};
+				closure2[]={};
+				soundClosure[]={};
+			};
+			class StandardSound: BaseSoundModeType
+			{
+				weaponSoundEffect="";
+				begin1[]=
+				{
+					"LF_Weapon_Unit\westarm5\sounds\westarm5_shot.ogg",
+					1,
+					1,
+					1800
+				};
+				soundBegin[]=
+				{
+					"begin1",
+					0.33000001
+				};
+			};
+			reloadTime=0.10;
+			dispersion=0.015;
+			minRange=5;
+			minRangeProbab=0.30000001;
+			midRange=25;
+			midRangeProbab=0.60000002;
+			maxRange=50;
+			maxRangeProbab=0.1;
+			aiRateOfFire=2;
+			aiRateOfFireDistance=25;
+		};
+		class close: Single
+		{
+			showToPlayer=0;
+			aiRateOfFire=0.25;
+			aiRateOfFireDistance=400;
+			minRange=0;
+			minRangeProbab=0.050000001;
+			midRange=200;
+			midRangeProbab=0.69999999;
+			maxRange=400;
+			maxRangeProbab=0.2;
+		};
+		class short: close
+		{
+			aiRateOfFire=0.5;
+			aiRateOfFireDistance=500;
+			minRange=300;
+			minRangeProbab=0.2;
+			midRange=400;
+			midRangeProbab=0.69999999;
+			maxRange=500;
+			maxRangeProbab=0.2;
+		};
+		class medium: close
+		{
+			aiRateOfFire=1;
+			aiRateOfFireDistance=900;
+			minRange=400;
+			minRangeProbab=0.2;
+			midRange=700;
+			midRangeProbab=0.69999999;
+			maxRange=900;
+			maxRangeProbab=0.2;
+		};
+		inertia=0.30000001;
+		dexterity=1.7;
+		initSpeed=500;
+		class OpticsModes
+		{
+			class sight
+			{
+				opticsID=1;
+				useModelOptics=1;
+				opticsPPEffects[]=
+				{
+					""
+				};
+				opticsDisablePeripherialVision=0;
+				opticsZoomMin=0.25;
+				opticsZoomMax=0.5;
+				opticsZoomInit=0.75;
+				discreteInitIndex=0;
+				distanceZoomMin=200;
+				distanceZoomMax=200;
+				memoryPointCamera="eye";
+				visionMode[]={};
+				opticsFlare="false";
+				cameraDir="";
+			};
+			class scope: sight
+			{
+				opticsID=2;
+				opticsZoomMin=0.125;
+				opticsZoomMax=0.125;
+				opticsZoomInit=0.125;
+				distanceZoomMin=300;
+				distanceZoomMax=300;
+			};
+		};
+		class WeaponSlotsInfo: WeaponSlotsInfo
+		{
+			mass=80;
+			class Cowsslot: Cowsslot
+			{
+				compatibleItems[]=
+				{
+					"Aux24th_WestarM5_scope",
+					"Aux24th_westarm5aholo_scope"
+				};
+			};
+		};
+	};
+	class UGL_F;
+	class Aux24th_WestarM5_GL: Aux24th_WestarM5
+	{
+		muzzles[] = {"this","UGL"};
+		scope=2;
+		model="LF_Weapon_Unit\westarm5\westarm5gl.p3d";
+		handAnim[]=
+		{
+			"OFP2_ManSkeleton",
+			"LF_Weapon_Unit\westarm5a\anims\westarm5a2.rtm"
+		};
+		reloadAction="ReloadMagazine";
+		picture="LF_Weapon_Unit\westarm5a\ui\westarm5standard.paa";
+		magazines[]=
+		{
+			"Aux24th_WestarM5_Mag"
+		};
+		displayname="Westar M5 with GL";
+		descriptionShort="Modifided Westar-M5 with GL";
+		selectionFireAnim="zasleh";
+		class UGL: UGL_F
+	    {
+			displayName = "[Aux24th] Westar-M5-GL";
+			descriptionShort = "GL for the Westar-M5 Platform";
+			useModelOptics = 0;
+			useExternalOptic = 0;
+			magazines[] = {"Aux24th_WestarM5_Mag_GL"};
+			magazineWell[] = {};
+			cameraDir = "OP_look";
+			discreteDistance[] = {100, 200, 300, 400};
+			discreteDistanceCameraPoint[] = {"OP_eye", "OP_eye2", "OP_eye3", "OP_eye4"};
+			discreteDistanceInitIndex = 1;
+			reloadAction = "ReloadMagazine";
+			reloadMagazineSound[] = {"LF_Weapon_Unit\main\sounds\dc15_reload.wss",5,1,10};
+			drySound[]={"LF_Weapon_Unit\main\sounds\dc15_empty.wss"};
+		};
+		class Single: Mode_SemiAuto
+		{
+			sounds[]=
+			{
+				"StandardSound"
+			};
+			class BaseSoundModeType
+			{
+				weaponSoundEffect="";
+				closure1[]={};
+				closure2[]={};
+				soundClosure[]={};
+			};
+			class StandardSound: BaseSoundModeType
+			{
+				weaponSoundEffect="";
+				begin1[]=
+				{
+					"LF_Weapon_Unit\westarm5\sounds\westarm5_shot.ogg",
+					1,
+					1,
+					1800
+				};
+				soundBegin[]=
+				{
+					"begin1",
+					0.33000001
+				};
+			};
+			reloadTime=0.45;
+			dispersion=0.002;
+			minRange=5;
+			minRangeProbab=0.30000001;
+			midRange=25;
+			midRangeProbab=0.60000002;
+			maxRange=50;
+			maxRangeProbab=0.1;
+			aiRateOfFire=2;
+			aiRateOfFireDistance=25;
+		};
+		class FullAuto: Mode_FullAuto
+		{
+			sounds[]=
+			{
+				"StandardSound"
+			};
+			class BaseSoundModeType
+			{
+				weaponSoundEffect="";
+				closure1[]={};
+				closure2[]={};
+				soundClosure[]={};
+			};
+			class StandardSound: BaseSoundModeType
+			{
+				weaponSoundEffect="";
+				begin1[]=
+				{
+					"LF_Weapon_Unit\westarm5\sounds\westarm5_shot.ogg",
+					1,
+					1,
+					1800
+				};
+				soundBegin[]=
+				{
+					"begin1",
+					0.33000001
+				};
+			};
+			reloadTime=0.15;
+			dispersion=0.005;
+			minRange=5;
+			minRangeProbab=0.30000001;
+			midRange=25;
+			midRangeProbab=0.60000002;
+			maxRange=50;
+			maxRangeProbab=0.1;
+			aiRateOfFire=2;
+			aiRateOfFireDistance=25;
+		};
+		class close: Single
+		{
+			showToPlayer=0;
+			aiRateOfFire=0.25;
+			aiRateOfFireDistance=400;
+			minRange=0;
+			minRangeProbab=0.050000001;
+			midRange=200;
+			midRangeProbab=0.69999999;
+			maxRange=400;
+			maxRangeProbab=0.2;
+		};
+		class short: close
+		{
+			aiRateOfFire=0.5;
+			aiRateOfFireDistance=500;
+			minRange=300;
+			minRangeProbab=0.2;
+			midRange=400;
+			midRangeProbab=0.69999999;
+			maxRange=500;
+			maxRangeProbab=0.2;
+		};
+		class medium: close
+		{
+			aiRateOfFire=1;
+			aiRateOfFireDistance=900;
+			minRange=400;
+			minRangeProbab=0.2;
+			midRange=700;
+			midRangeProbab=0.69999999;
+			maxRange=900;
+			maxRangeProbab=0.2;
+		};
+		inertia=0.30000001;
+		dexterity=1.7;
+		initSpeed=500;
+	};
+	class ItemCore;
+	class InventoryOpticsItem_Base_F;
+	class Aux24th_WestarM5_scope: ItemCore
+	{
+		displayName="Westar M-5 scope";
+		author="Last Force Project";
+		picture="LF_Weapon_Unit\westarm5a\ui\sniper_scope.paa";
+		model="LF_Weapon_Unit\westarm5\westarm5scope.p3d";
+		scope=2;
+		descriptionShort="";
+		weaponInfoType="RscWeaponZeroing";
+		class ItemInfo: InventoryOpticsItem_Base_F
+		{
+			mass=8;
+			opticType=1;
+			optics=1;
+			modelOptics="LF_Weapon_Unit\westarm5\westarm5optic.p3d";
+			class OpticsModes
+			{
+				class WestarM5scope_sights
+				{
+					opticsID=1;
+					useModelOptics=0;
+					opticsPPEffects[]=
+					{
+						"Default"
+					};
+					opticsFlare=0;
+					opticsDisablePeripherialVision=0;
+					opticsZoomMin=0.25;
+					opticsZoomMax=1.25;
+					opticsZoomInit=0.75;
+					memoryPointCamera="eye";
+					visionMode[]={};
+					distanceZoomMin=200;
+					distanceZoomMax=200;
+					cameraDir="";
+				};
+				class WestarM5scope_scope: WestarM5scope_sights
+				{
+					opticsID=1;
+					useModelOptics=1;
+					opticsPPEffects[]=
+					{
+						"OpticsCHAbera2",
+						"OpticsBlur3"
+					};
+					opticsZoomMin=0.0625;
+					opticsZoomMax=0.125;
+					opticsZoomInit=0.125;
+					discreteDistance[]={100,200,300,400,500,600,700,800,900,1000,1100,1200};
+					discreteDistanceInitIndex=1;
+					distanceZoomMin=300;
+					distanceZoomMax=1200;
+					discretefov[]=
+					{
+						"0.25/3",
+						"0.25/6",
+						"0.25/9",
+						"0.25/12",
+						"0.25/15",
+						"0.25/20"
+					};
+					discreteInitIndex=0;
+					memoryPointCamera="opticView";
+					visionMode[]=
+					{
+						"Normal",
+						"NVG",
+						"TI"
+					};
+					opticsFlare=1;
+					opticsDisablePeripherialVision=1;
+					cameraDir="";
+				};
+			};
+		};
+		inertia=0.1;
+	};
 };
